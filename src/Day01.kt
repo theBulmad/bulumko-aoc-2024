cf087fc72ec59104b09bc1dced4f33488432cd07
Bulumko Madikane <thebulmad@gmail.com>
2024-12-01 21:05:46 +0200

    feat: Added Day01 solution (#1)

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,35 @@
+import kotlin.math.abs
+
 fun main() {
+
+    fun splitInput(input: List<String>): Pair<List<Int>, List<Int>> =
+        input.map {it.split("   ").let { parts -> parts[0].toInt() to parts[1].toInt()}}.unzip()
+
     fun part1(input: List<String>): Int {
-        return input.size
+
+        val (firstList, secondList) = splitInput(input)
+
+        return firstList.sortedBy { it }.zip(secondList.sortedBy { it })
+            .sumOf { abs(it.first - it.second) }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+
+        val (firstList, secondList) = splitInput(input)
+
+        return firstList
+            .sumOf {
+                it * secondList.filter { e -> e == it }.size
+            }
+
     }
 
     // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+    //check(part1(listOf("test_input")) == 1)
 
     // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+//    val testInput = readInput("Day01_test")
+//    check(part1(testInput) == 1)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
